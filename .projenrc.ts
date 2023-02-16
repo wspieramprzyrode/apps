d85dabc4471c8e114a16eb7a63e47c47ef3d0168
Wojciech Wójcik <wwojcik@MacBook-Pro-Wojciech.local>
2023-02-16 08:46:14 +0100

    initial commit

---
+++ .projenrc.ts
@@ -0,0 +1,15 @@
+import { nx_monorepo } from "aws-prototyping-sdk";
+const authorName = "Wspieram Przyrodę";
+const authorEmail = "team@wspieramprzyrode.pl";
+const repository = "https://github.com/wspieramprzyrode/apps";
+const project = new nx_monorepo.NxMonorepoProject({
+  defaultReleaseBranch: "main",
+  devDeps: ["aws-prototyping-sdk"],
+  name: "wspieram-przyrode",
+  authorName: authorName,
+  authorEmail: authorEmail,
+  authorOrganization: true,
+  repository: repository,
+  vscode: true,
+});
+project.synth();
---
+++ README.md
@@ -0,0 +1,1 @@
+# Wspieram Przyrodę
